177f936a43a06748288c146c6972f848799497f0
tabish <tabish@tabish.pcvark.in>
2023-06-06 15:03:03 +0530

    June First commit

---
+++ junegit/src/test/java/junegit/test0.java
@@ -0,0 +1,11 @@
+package junegit;
+
+public class test0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, welcome to GIT......");
+
+	}
+
+}
