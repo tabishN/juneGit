b40031c90c8af13e582d1614e1848e2c902e7762
tabish <tabish@tabish.pcvark.in>
2023-07-18 14:15:07 +0530

    Test class add

---
+++ junegit/src/test/java/junegit/mTest.java
@@ -0,0 +1,25 @@
+package junegit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class mTest {
+	WebDriver driver;
+
+	@Test
+	public void metd() {
+		System.out.println("Hello welcome to Jenkins");
+		Reporter.log("Logs easily accessable", true);
+		System.out.println("Initilizing the Google Chrome Driver");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Reporter.log("Chrome launched successfully", true);
+		driver.get("https://otest.anytimeastro.com/");
+		Reporter.log("site open successfully", true);
+		driver.quit();
+	}
+
+}
