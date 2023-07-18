78a91091c78b950bf79146104184f3396fdec25d
tabish <tabish@tabish.pcvark.in>
2023-07-18 12:05:26 +0530

    maven jenkins integration

--- junegit/src/test/java/junegit/test0.java
+++ junegit/src/test/java/junegit/test0.java
@@ -1,11 +1,25 @@
 package junegit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class test0 {
+	WebDriver driver;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, welcome to GIT......");
-
+	@Test
+	public void metd() {
+		System.out.println("Hello welcome to Jenkins");
+		Reporter.log("Logs easily accessable", true);
+		System.out.println("Initilizing the Google Chrome Driver");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Reporter.log("Chrome launched successfully", true);
+		driver.get("https://otest.anytimeastro.com/");
+		Reporter.log("site open successfully", true);
+		driver.quit();
 	}
 
 }
